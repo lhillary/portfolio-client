e80e2885908458dddb5dd9622edc92f9f69f4def
Lydia Hillary <lydia.hillary@gmail.com>
2021-03-02 16:40:50 -0500

    changed API url since deployment

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -11,7 +11,7 @@ export default {
     };
 
     try{
-        let res = await axios.post('http://localhost:8000/contacts', contact, axiosConfig);
+        let res = await axios.post('https://lydiahillary-portfolio-api.herokuapp.com/contacts', contact, axiosConfig);
         console.log(res.data);
     } catch(err) {
         console.log(err);
