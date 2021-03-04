08c5dff98287c94b228e8bccc5b0a03b4709e21f
Lydia Hillary <lydia.hillary@gmail.com>
2021-03-03 22:58:18 -0500

    changed verbiage

--- src/views/About.vue
+++ src/views/About.vue
@@ -18,7 +18,7 @@
         smallcaps: 'A Bit About Me And My Experience',
         paragraphs: [
           {
-            text: 'Front-End web developer previously in web UI/UX design. Attended Detroit Grand Circus bootcamp in 2018 to bridge my design and development skills and have been working in web development professionally since. Focused on combining my development knowledge with my passion for design to deliver beautiful and well-designed web experiences.'
+            text: 'Front-End web developer previously in web UI/UX design. Attended Detroit Grand Circus bootcamp in 2019 to bridge my design and development skills and have been working in web development professionally since. Focused on combining my development knowledge with my passion for design to deliver beautiful and well-designed web experiences.'
           },{
             text: 'One of the things I enjoy most about web development is the limitless opportunities for learning; being able to tool around with something new and shiny (so frequently) is something that I consider to be a professional privilege. I am currently working on learning more about the AWS environment, and also finally diving into React after getting a decent grasp on Vue.js.'
           }
